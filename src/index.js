652cac5f09164742dca2574c08c204941d5e5a7a
LuciaBOURQUE <80245094+LuciaBOURQUE@users.noreply.github.com>
2023-09-09 11:43:17 +0200

    Readme edit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Nav from "./components/Navbar/Nav.jsx"
+import Footer from "./components/Footer/Footer"
+
 import Home from "./pages/Home/Home.jsx"
 import About from "./pages/About/About.jsx"
-import Footer from "./components/Footer/Footer"
 import Error404 from "./pages/404Error/Error.jsx"
 import Accomodation from "./pages/Accomodation/Accomodation.jsx"
 import { Lodging } from "./datas/lodging.js"
